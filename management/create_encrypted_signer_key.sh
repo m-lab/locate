eca05f2b23cc7bb0fd95e1f8ecce8ead927a6904
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-04-02 11:28:40 -0400

    Produce per-project environment variable

--- management/create_encrypted_signer_key.sh
+++ management/create_encrypted_signer_key.sh
@@ -75,9 +75,13 @@ ENC_SIGNER_KEY=$( cat ${PRIVATE} | gcloud kms encrypt \
   --keyring=${KEYRING} \
   --key=${KEY} | base64 )
 
+# Replace all hyphens in project name with underscore to create a valid
+# environment variable.
+project=${PROJECT//-/_}
+
 echo ""
 echo ""
 echo "Include the following in app.yaml:"
 echo ""
 echo "env_variables:"
-echo "  ENCRYPTED_SIGNER_KEY: \"${ENC_SIGNER_KEY}\""
+echo "  ENCRYPTED_SIGNER_KEY_${project}: \"${ENC_SIGNER_KEY}\""
