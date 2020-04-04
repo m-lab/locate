38b2c6ba26ad142e581eb9175bd57f073acb0ede
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-04-03 23:37:26 -0400

    Add monitoring handler with unit tests

---
+++ handler/monitoring.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/m-lab/access/controller"
+	"github.com/m-lab/go/host"
+	v2 "github.com/m-lab/locate/api/v2"
+	"github.com/m-lab/locate/static"
+)
+
+// Monitoring implements /v2/monitoring requests.
+func (c *Client) Monitoring(rw http.ResponseWriter, req *http.Request) {
+	result := v2.MonitoringResult{}
+
+	// Validate request.
+	cl := controller.GetClaim(req.Context())
+	if cl == nil { // && requireClaim {
+		result.Error = v2.NewError("claim", "Must provide access_token", http.StatusBadRequest)
+		writeResult(rw, &result)
+		return
+	}
+
+	// Check that the given subject appears to be an M-Lab machine name.
+	_, err := host.Parse(cl.Subject)
+	if err != nil {
+		result.Error = v2.NewError("subject", "Subject must be specified", http.StatusBadRequest)
+		writeResult(rw, &result)
+		return
+	}
+
+	// Lookup service configuration.
+	experiment, datatype := getDatatypeAndExperiment(req.URL.Path)
+	service := experiment + "/" + datatype
+	ports, ok := static.Configs[service]
+	if !ok {
+		result.Error = v2.NewError("config", "Unknown service: "+service, http.StatusBadRequest)
+		writeResult(rw, &result)
+		return
+	}
+
+	// Get monitoring subject access tokens for the given machine.
+	machine := cl.Subject
+	urls := c.getURLs(ports, machine, experiment, static.SubjectMonitoring)
+	result.Results = []v2.Target{
+		// Monitoring results only include one target.
+		{Machine: machine, URLs: urls},
+	}
+	writeResult(rw, &result)
+}
---
+++ handler/monitoring_test.go
@@ -0,0 +1,118 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"gopkg.in/square/go-jose.v2/jwt"
+
+	"github.com/go-test/deep"
+	"github.com/m-lab/access/controller"
+	"github.com/m-lab/go/rtx"
+	v2 "github.com/m-lab/locate/api/v2"
+	"github.com/m-lab/locate/static"
+)
+
+func TestClient_Monitoring(t *testing.T) {
+	tests := []struct {
+		name    string
+		claim   *jwt.Claims
+		signer  Signer
+		locator Locator
+		path    string
+		wantErr *v2.Error
+	}{
+		{
+			name: "success-machine",
+			claim: &jwt.Claims{
+				Issuer:   static.IssuerMonitoring,
+				Subject:  "mlab1-lga0t.mlab-oti.measurement-lab.org",
+				Audience: jwt.Audience{static.AudienceLocate},
+				Expiry:   jwt.NewNumericDate(time.Now().Add(time.Minute)),
+			},
+			signer: &fakeSigner{},
+			locator: &fakeLocator{
+				machines: []string{"mlab1-lga0t.measurement-lab.org"},
+			},
+			path: "ndt/ndt5",
+		},
+		{
+			name:  "error-no-claim",
+			claim: nil,
+			path:  "ndt/ndt5",
+			wantErr: &v2.Error{
+				Type:   "claim",
+				Title:  "Must provide access_token",
+				Status: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "error-bad-subject",
+			claim: &jwt.Claims{
+				Issuer:   static.IssuerMonitoring,
+				Subject:  "this-is-an-invalid-hostname",
+				Audience: jwt.Audience{static.AudienceLocate},
+				Expiry:   jwt.NewNumericDate(time.Now().Add(time.Minute)),
+			},
+			path: "ndt/ndt5",
+			wantErr: &v2.Error{
+				Type:   "subject",
+				Title:  "Subject must be specified",
+				Status: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "error-invalid-service-path",
+			claim: &jwt.Claims{
+				Issuer:   static.IssuerMonitoring,
+				Subject:  "mlab1-lga0t.mlab-oti.measurement-lab.org",
+				Audience: jwt.Audience{static.AudienceLocate},
+				Expiry:   jwt.NewNumericDate(time.Now().Add(time.Minute)),
+			},
+			path: "ndt/this-is-an-invalid-service-name",
+			wantErr: &v2.Error{
+				Type:   "config",
+				Title:  "Unknown service: ndt/this-is-an-invalid-service-name",
+				Status: http.StatusBadRequest,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := NewClient("mlab-sandbox", tt.signer, tt.locator)
+			rw := httptest.NewRecorder()
+			req := httptest.NewRequest(http.MethodGet, "/v2/monitoring/"+tt.path, nil)
+			req = req.Clone(controller.SetClaim(req.Context(), tt.claim))
+
+			c.Monitoring(rw, req)
+
+			q := v2.MonitoringResult{}
+			err := json.Unmarshal(rw.Body.Bytes(), &q)
+			rtx.Must(err, "Failed to unmarshal")
+
+			if tt.wantErr != nil {
+				if q.Error == nil {
+					t.Fatal("Monitoring() expected error, got nil")
+				}
+				if diff := deep.Equal(q.Error, tt.wantErr); diff != nil {
+					t.Errorf("Monitoring() expected error: got: %#v", diff)
+				}
+				return
+			}
+			if len(q.Results) > 1 {
+				t.Errorf("Monitoring() returned wrong result count; got %d, want 1", len(q.Results))
+			}
+			if q.Results[0].Machine != tt.claim.Subject {
+				t.Errorf("Monitoring() returned different machine than claim subject; got %s, want %s",
+					q.Results[0].Machine, tt.claim.Subject)
+			}
+			if len(q.Results[0].URLs) != len(static.Configs[tt.path]) {
+				t.Errorf("Monitoring() returned incomplete urls; got %d, want %d",
+					len(q.Results[0].URLs), len(static.Configs[tt.path]))
+			}
+		})
+	}
+}
