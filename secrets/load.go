a0292f951cc5069931f6abbb16e0656418fe8fbd
Nathan Kinkade <kinkade@measurementlab.net>
2021-10-20 15:28:10 -0600

    Causes oldest enabled secret version to be used
    Previously, the newest enabled secret version was selected, but this
    caused a possible failure mode where the Locate Service could load a new
    private key before the corresponding public key has been deployed to all
    relevant services. Selecting the oldest enabled key means that operators
    can add new keys at will, and they will not become active until all
    older enabled versions have first been disabled.

--- secrets/load.go
+++ secrets/load.go
@@ -94,14 +94,14 @@ func (c *Config) getSecretVersions(ctx context.Context, client SecretClient) ([]
 	return versions, nil
 }
 
-// LoadSigner fetches the latest version of the named secret containing the JWT
-// signer key from the Secret Manager API and returns a *token.Signer.
+// LoadSigner fetches the oldest enabled version of the named secret containing
+// the JWT signer key from the Secret Manager API and returns a *token.Signer.
 func (c *Config) LoadSigner(ctx context.Context, client SecretClient) (*token.Signer, error) {
 	versions, err := c.getSecretVersions(ctx, client)
 	if err != nil {
 		return nil, err
 	}
-	key, err := c.getSecret(ctx, client, versions[0])
+	key, err := c.getSecret(ctx, client, versions[len(versions)-1])
 	if err != nil {
 		return nil, err
 	}
