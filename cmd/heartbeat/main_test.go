61fd0bc5721890e1f300b591a6ab8d56160e1285
Cristina Leon <cristinaleon@google.com>
2022-04-29 19:27:33 +0000

    Test coverage increase

--- cmd/heartbeat/main_test.go
+++ cmd/heartbeat/main_test.go
@@ -1,31 +1,31 @@
 package main
 
-// import (
-// 	"context"
-// 	"flag"
-// 	"testing"
-// 	"time"
+import (
+	"context"
+	"flag"
+	"testing"
+	"time"
 
-// 	"github.com/m-lab/locate/connection/testdata"
-// )
+	"github.com/m-lab/locate/connection/testdata"
+)
 
-// func Test_main(t *testing.T) {
-// 	mainCtx, mainCancel = context.WithCancel(context.Background())
-// 	fh := testdata.FakeHandler{}
-// 	s := testdata.FakeServer(fh.Upgrade)
-// 	defer s.Close()
+func Test_main(t *testing.T) {
+	mainCtx, mainCancel = context.WithCancel(context.Background())
+	fh := testdata.FakeHandler{}
+	s := testdata.FakeServer(fh.Upgrade)
+	defer s.Close()
 
-// 	flag.Set("locate-url", s.URL)
+	flag.Set("locate-url", s.URL)
 
-// 	timer := time.NewTimer(time.Second)
-// 	go func() {
-// 		<-timer.C
-// 		if locate != s.URL {
-// 			t.Errorf("main() incorrect locate URL; got: %s, want: %s",
-// 				locate, s.URL)
-// 		}
-// 		mainCancel()
-// 	}()
+	timer := time.NewTimer(time.Second)
+	go func() {
+		<-timer.C
+		if locate != s.URL {
+			t.Errorf("main() incorrect locate URL; got: %s, want: %s",
+				locate, s.URL)
+		}
+		mainCancel()
+	}()
 
-// 	main()
-// }
+	main()
+}
