18af0d8fc0270f277e684245b8e9c82bd6aae162
Cristina Leon <cristinaleon@google.com>
2022-08-02 19:11:21 +0000

    Implement port probing for HBS

--- cmd/heartbeat/main.go
+++ cmd/heartbeat/main.go
@@ -42,8 +42,9 @@ func main() {
 	r, err := LoadRegistration(mainCtx, hostname, registrationURL.URL)
 	rtx.Must(err, "could not load registration data")
 	// Populate flag values.
+	s := services.Get()
+	r.Services = s
 	r.Experiment = experiment
-	r.Services = services.Get()
 	hbm := v2.HeartbeatMessage{Registration: r}
 
 	// Establish a connection.
@@ -51,12 +52,15 @@ func main() {
 	err = conn.Dial(heartbeatURL, http.Header{}, hbm)
 	rtx.Must(err, "failed to establish a websocket connection with %s", heartbeatURL)
 
-	write(conn)
+	ps := NewPortScanner(s)
+	hc := NewHealthChecker(*ps)
+
+	write(conn, hc)
 }
 
 // write starts a write loop to send health messages every
 // HeartbeatPeriod.
-func write(ws *connection.Conn) {
+func write(ws *connection.Conn, hc *HealthChecker) {
 	defer ws.Close()
 	ticker := *time.NewTicker(heartbeatPeriod)
 	defer ticker.Stop()
@@ -66,7 +70,8 @@ func write(ws *connection.Conn) {
 		case <-mainCtx.Done():
 			return
 		case <-ticker.C:
-			healthMsg := v2.Health{Score: 1.0}
+			score := hc.getHealth()
+			healthMsg := v2.Health{Score: score}
 			hbm := v2.HeartbeatMessage{Health: &healthMsg}
 
 			err := ws.WriteMessage(websocket.TextMessage, hbm)
