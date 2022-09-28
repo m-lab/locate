baa5c83eedba354735534ae12fa434055dcc40d3
Cristina Leon <cristinaleon@google.com>
2022-09-28 01:13:57 +0000

    Fix wording

--- cmd/heartbeat/health/endpoint-check.go
+++ cmd/heartbeat/health/endpoint-check.go
@@ -11,12 +11,12 @@ var (
 )
 
 // checkHealthEndpoint makes a call to the the local /health endpoint.
-// It returns an error if there was an HTTP protocol error.
+// It returns an error if the HTTP request was not successful.
 // It returns true only if the returned HTTP status code equals 200 (OK).
 func checkHealthEndpoint() (bool, error) {
 	resp, err := http.Get(healthAddress)
 	if err != nil {
-		metrics.HealthEndpointChecksTotal.WithLabelValues("HTTP protocol error").Inc()
+		metrics.HealthEndpointChecksTotal.WithLabelValues("HTTP request error").Inc()
 		return false, err
 	}
 
