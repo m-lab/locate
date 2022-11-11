b0df894800830e5a555e1ed853a4224c97a30d21
Cristina Leon <cristinaleon@google.com>
2022-11-11 21:49:27 +0000

    Make Kubernetes error generic

--- cmd/heartbeat/health/kubernetes-client.go
+++ cmd/heartbeat/health/kubernetes-client.go
@@ -2,6 +2,7 @@ package health
 
 import (
 	"context"
+	"log"
 	"net/url"
 	"path"
 	"strconv"
@@ -15,6 +16,8 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 )
+
+var errKubernetesAPI = "error making request to Kubernetes API server"
 
 // KubernetesClient manages requests to the Kubernetes API server.
 type KubernetesClient struct {
@@ -101,7 +104,8 @@ func (c *KubernetesClient) isHealthy(ctx context.Context) bool {
 func (c *KubernetesClient) isPodRunning(ctx context.Context) bool {
 	pod, err := c.clientset.CoreV1().Pods(c.namespace).Get(ctx, c.pod, metav1.GetOptions{})
 	if err != nil {
-		metrics.KubernetesRequestsTotal.WithLabelValues(err.Error()).Inc()
+		log.Printf("%s: %v", errKubernetesAPI, err)
+		metrics.KubernetesRequestsTotal.WithLabelValues(errKubernetesAPI).Inc()
 		return true
 	}
 
@@ -117,7 +121,8 @@ func (c *KubernetesClient) isPodRunning(ctx context.Context) bool {
 func (c *KubernetesClient) isNodeReady(ctx context.Context) bool {
 	node, err := c.clientset.CoreV1().Nodes().Get(ctx, c.node, metav1.GetOptions{})
 	if err != nil {
-		metrics.KubernetesRequestsTotal.WithLabelValues(err.Error()).Inc()
+		log.Printf("%s: %v", errKubernetesAPI, err)
+		metrics.KubernetesRequestsTotal.WithLabelValues(errKubernetesAPI).Inc()
 		return true
 	}
 
