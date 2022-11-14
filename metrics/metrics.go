a92b36ba6c65435efe2fae7e9c7a2917b8822c13
Cristina Leon <cristinaleonr@users.noreply.github.com>
2022-11-14 16:02:36 -0500

    Remove buckets from histogram metric (#89)

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -88,9 +88,8 @@ var (
 	// Service to the Kubernetes API server (in seconds).
 	KubernetesRequestTimeHistogram = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "heartbeat_kubernetes_request_time_histogram",
-			Help:    "Request time from the HBS to the Kubernetes API server (seconds)",
-			Buckets: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			Name: "heartbeat_kubernetes_request_time_histogram",
+			Help: "Request time from the HBS to the Kubernetes API server (seconds)",
 		},
 		[]string{"healthy"},
 	)
