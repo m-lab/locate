51d99b39f608b5ff973914d435ab0d9d28bcb008
Cristina Leon <cristinaleon@google.com>
2023-02-23 21:15:40 +0000

    Fix bucket width

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -84,7 +84,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "locate_server_distance_ranking",
 			Help:    "A histogram of server selection rankings with respect to distance from the client.",
-			Buckets: prometheus.LinearBuckets(0, 1000, 1),
+			Buckets: prometheus.LinearBuckets(0, 1, 1000),
 		},
 		[]string{"index"},
 	)
