3216ec3daad2722d19845d65eca0e7c38d462822
Cristina Leon <cristinaleon@google.com>
2023-03-03 20:58:30 +0000

    Skip site probabilities for machine-type=virtual queries

--- heartbeat/location.go
+++ heartbeat/location.go
@@ -111,7 +111,9 @@ func filterSites(service string, lat, lon float64, instances map[string]v2.Heart
 
 	sites := make([]site, 0)
 	for _, v := range m {
-		if pickWithProbability(v.registration.Site) {
+		// Virtual sites do not need further filtering if the query is already requesting
+		// only virtual machines.
+		if pickWithProbability(v.registration.Site) || opts.Type == "virtual" {
 			sites = append(sites, *v)
 		}
 	}
