5164f5d436ce84da4c55b43e0bf0740bf877b1f8
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-05-08 14:49:25 -0400

    Reduce ATL metro probability to .5

--- static/configs.go
+++ static/configs.go
@@ -82,6 +82,14 @@ var LegacyServices = map[string]string{
 // TODO(github.com/m-lab/locate/issues/92): Make this dynamic.
 var SiteProbability = map[string]float64{
 	"ams10": 0.3, // virtual site
+
+	// TODO(soltesz): revert ATL/CHS trial after 2023-05-15.
+	"atl02": 0.5,
+	"atl03": 0.5,
+	"atl04": 0.5,
+	"atl07": 0.5,
+	"atl08": 0.5,
+
 	"bom01": 0.1,
 	"bom02": 0.1,
 	"bru06": 0.3, // virtual site
