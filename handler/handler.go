1703c0cc99f7864897db8f1003ebd6ade67b492e
Cristina Leon <cristinaleon@google.com>
2023-10-01 22:15:57 +0000

    Debug

--- handler/handler.go
+++ handler/handler.go
@@ -355,6 +355,8 @@ func (c *Client) getURLs(ports static.Ports, machine, experiment, token string, 
 
 func allowRequest(now time.Time, req *http.Request) bool {
 	agent := req.Header.Get("User-Agent")
+	log.Println("User agent: ", agent)
+	log.Println("Headers: ", req.Header)
 	// TODO(cristinaleon): Change after test.
 	if agent != "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36" {
 		return true
