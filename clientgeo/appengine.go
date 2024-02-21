96fe57d459e648ce5f0f2e3ecd9b2cdd0727970b
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-02-21 16:32:20 -0500

    Add comment and todo

--- clientgeo/appengine.go
+++ clientgeo/appengine.go
@@ -18,6 +18,7 @@ var (
 	// ErrNullLatLon is returned with a 0,0 lat/lon value is provided.
 	ErrNullLatLon = errors.New("lat,lon value was null: " + nullLatLon)
 
+	// ErrUserIPOverride is returned when a request includes the "ip=" parameter.
 	ErrUserIPOverride = errors.New("skipped app engine for user ip parameter")
 
 	latlonMethod  = "appengine-latlong"
@@ -51,6 +52,7 @@ func (sl *AppEngineLocator) Locate(req *http.Request) (*Location, error) {
 	// Allow user-provide ip to override app engine locations.
 	// NOTE: this depends on later client geo locators interpreting
 	// the ip parameter.
+	// TODO(soltesz): remove once v1 resources are removed.
 	if req.URL.Query().Get("ip") != "" {
 		return nil, ErrUserIPOverride
 	}
