159efd93724b40144575b20f7dd29142dde94499
Roberto D'Auria <roberto@measurementlab.net>
2025-02-09 22:00:38 +0100

    Fix flag name

--- locate.go
+++ locate.go
@@ -80,7 +80,7 @@ func init() {
 	flag.DurationVar(&rateLimitInterval, "rate-limit-interval", time.Hour, "Time window for IP+UA rate limiting")
 	flag.IntVar(&rateLimitMax, "rate-limit-max", 5, "Max number of events in the time window for IP+UA rate limiting")
 	flag.StringVar(&rateLimitPrefix, "rate-limit-prefix", "locate:ratelimit:", "Prefix for Redis keys for IP+UA rate limiting")
-	flag.StringVar(&rateLimitRedisAddr, "rate-limit-redis-addr", "", "Primary endpoint for Redis instance for rate limiting")
+	flag.StringVar(&rateLimitRedisAddr, "rate-limit-redis-address", "", "Primary endpoint for Redis instance for rate limiting")
 	// Enable logging with line numbers to trace error locations.
 	log.SetFlags(log.LUTC | log.Llongfile)
 }
