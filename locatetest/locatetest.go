13aef12ba289b81c50100cb31a68d4a95ec9cc57
Simone Basso <bassosimone@gmail.com>
2026-01-15 18:12:26 +0100

    refactor(locatetest)!: allow returning valid URLs (#240)
    The current implementation of the `locatetest` package does not allow to return valid URLs. As such, mocking a realistic locate response is not possible and testing is less realistic.
    
    To address this, I propose to provide the `*TargetInfo` to the fake server directly. This design allows for maximum flexibility at the cost of slightly more sweating in the tests that use this package, for they would need to provide a fully populated response. (Though I added a constructor for the common case of `ndt7`, which can also serve as a reference implementation for creating the proper mocks elsewhere.)
    
    Yet, this is probably the most flexible compromise in terms of decoupling the ability to run a local, fake locate and specifying what each specific user of `locatetest` needs.
    
    BREAKING CHANGE: users of `locatetest.LocatorV2` now need to specify the full `*TargetInfo` as opposed to just passing the list of servers or use the convenience `NewTargetInfoNdt7` constructor (if that fits your needs). The following code illustrates a reasonable upgrade path that produces a valid ndt7 response:
    
    ```Go
    // Change this
    server := locatetest.NewLocateServerV2(&locatetest.LocatorV2{
        Servers: []string{"127.0.0.1"},
    })
    
    // Into this
    server := locatetest.NewLocateServerV2(&locatetest.LocatorV2{
        TargetInfo: locatetest.NewTargetInfoNdt7("127.0.0.1"),
    })
    ```
    
    If you need to mock a `/v2/nearest` response for a different experiment, you should adapt `NewTargetInfoNdt7` to your use case. In such a case, we recommend comparing to the actual locate output to make the response realistic.

--- locatetest/locatetest.go
+++ locatetest/locatetest.go
@@ -1,6 +1,7 @@
 package locatetest
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -10,7 +11,7 @@ import (
 
 	"github.com/go-jose/go-jose/v4/jwt"
 
-	v2 "github.com/m-lab/locate/api/v2"
+	"github.com/m-lab/locate/api/v2"
 	"github.com/m-lab/locate/clientgeo"
 	"github.com/m-lab/locate/handler"
 	"github.com/m-lab/locate/heartbeat"
@@ -28,26 +29,45 @@ func (s *Signer) Sign(cl jwt.Claims) (string, error) {
 	return t, nil
 }
 
-// LocatorV2 is a fake LocatorV2 implementation that returns the configured Servers or Err.
+// LocatorV2 is a fake LocatorV2 implementation that returns the configured TargetInfo or Err.
 type LocatorV2 struct {
+	// StatusTracker is here just to satisfy the interface. You don't need to
+	// initialize this for typical usage of this code in testing.
 	heartbeat.StatusTracker
-	Servers []string
-	Err     error
+
+	// TargetInfo is the optional TargetInfo to return.
+	//
+	// If both TargetInfo and Err are nil, Nearest returns an error.
+	//
+	// If both are non-nil, Err takes precedence.
+	//
+	// The [NewTargetInfoNdt7] func allows to construct a suitable instance that
+	// makes sense for returning meaningful ndt7 entries.
+	TargetInfo *heartbeat.TargetInfo
+
+	// Err is the optional error to return.
+	//
+	// If both TargetInfo and Err are nil, Nearest returns an error.
+	//
+	// If both are non-nil, Err takes precedence.
+	Err error
 }
 
 // Nearest returns the pre-configured LocatorV2 Servers or Err.
 func (l *LocatorV2) Nearest(service string, lat, lon float64, opts *heartbeat.NearestOptions) (*heartbeat.TargetInfo, error) {
-	if l.Err != nil {
+	// Design choice: here we *could* do the right thing(tm) in terms of
+	// returning the correct structure for each selected service, however,
+	// it is more scalable that tests for a specific package do that.
+	switch {
+	case l.Err != nil:
 		return nil, l.Err
+
+	case l.TargetInfo != nil:
+		return l.TargetInfo, nil
+
+	default:
+		return nil, errors.New("locatorV2: expected non-nil TargetInfo or Err")
 	}
-	t := make([]v2.Target, len(l.Servers))
-	for i := range l.Servers {
-		t[i].Machine = l.Servers[i]
-	}
-	return &heartbeat.TargetInfo{
-		Targets: t,
-		URLs:    []url.URL{},
-	}, nil
 }
 
 // NewLocateServerV2 creates an httptest.Server that can respond to Locate API V2
@@ -64,4 +84,44 @@ func NewLocateServerV2(loc *LocatorV2) *httptest.Server {
 	srv := httptest.NewServer(mux)
 	log.Println("Listening for INSECURE access requests on " + srv.URL)
 	return srv
+}
+
+// NewTargetInfoNdt7 is a convenience function to build a [*heartbeat.TargetInfo] to
+// initialize a [*LocatorV2] type with targets meaningful for ndt7.
+//
+// This function serves as a reference implementation example regarding how to fill
+// a [*heartbeat.TestInfo] structure for testing. If you have specific needs for other
+// experiments hosted by M-Lab, you're probably better off adding the code you need
+// inside your implementation rather than inside this package.
+//
+// The returned response is not exactly the same as a response returned by locate
+// because we do not prefix the machine with `ndt-`, which allows to build correct
+// URLs regardless of whether machine is an FQDN, an IP address, or an endpoint
+// containing an IP address _and_ a port (which is the most common use case in testing).
+func NewTargetInfoNdt7(machines ...string) *heartbeat.TargetInfo {
+
+	// Build the targets first. The location does not seem so important and I assume someone
+	// who wants a more precise mock could just build the desired struct manually.
+	targets := make([]v2.Target, 0, len(machines))
+	for _, machine := range machines {
+		targets = append(targets, v2.Target{
+			Machine:  machine,
+			Hostname: machine,
+			Location: &v2.Location{
+				City:    "Ventimiglia",
+				Country: "IT",
+			},
+		})
+	}
+
+	// Assemble the result along with the standard ndt7 schemes.
+	return &heartbeat.TargetInfo{
+		Targets: targets,
+		URLs: []url.URL{
+			{Scheme: "ws", Host: "", Path: "/ndt/v7/download"},
+			{Scheme: "ws", Host: "", Path: "/ndt/v7/upload"},
+			{Scheme: "wss", Host: "", Path: "/ndt/v7/download"},
+			{Scheme: "wss", Host: "", Path: "/ndt/v7/upload"},
+		},
+	}
 }
--- locatetest/locatetest_test.go
+++ locatetest/locatetest_test.go
@@ -3,79 +3,206 @@ package locatetest
 import (
 	"context"
 	"errors"
-	"net/http/httptest"
+	"fmt"
 	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/m-lab/go/testingx"
 	"github.com/m-lab/locate/api/locate"
+	"github.com/m-lab/locate/api/v2"
 )
 
-func TestLocateServer_Success(t *testing.T) {
-	tests := []struct {
-		name string
-		srv  *httptest.Server
-		path string
-		want int
-	}{
-		{
-			name: "success-locate-server-v2",
-			srv: NewLocateServerV2(&LocatorV2{
-				Servers: []string{"127.0.0.1"},
-			}),
-			path: "/v2/nearest",
-			want: 1,
-		},
+// defines expectations for returned v2.Target URLs
+type v2TargetURLExpectation struct {
+	key         string
+	valuePrefix string
+}
+
+// defines what we expect v2.Target to be
+type v2TargetExpectation struct {
+	machine  string
+	hostname string
+	loc      *v2.Location
+	urls     []v2TargetURLExpectation
+}
+
+func (exp *v2TargetExpectation) check(t *testing.T, target *v2.Target) {
+	// Machine
+	if target.Machine != exp.machine {
+		t.Error("expected", exp.machine, "got", target.Machine)
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := locate.NewClient("fake-user-agent")
-			u, err := url.Parse(tt.srv.URL)
-			testingx.Must(t, err, "failed to parse locatetest url")
-			u.Path = tt.path
-			c.BaseURL = u
+	// Hostname
+	if target.Hostname != exp.hostname {
+		t.Error("expected", exp.hostname, "got", target.Hostname)
+	}
 
-			ctx := context.Background()
-			// NOTE: only known services (e.g. ndt/ndt7) are supported by the locate API.
-			targets, err := c.Nearest(ctx, "ndt/ndt7")
-			testingx.Must(t, err, "failed to get response from locatetest server")
+	// Location
+	switch {
+	case exp.loc == nil && target.Location == nil:
+		// nothing
 
-			if tt.want != len(targets) {
-				t.Errorf("NewLocateServer() = got %d, want %d", len(targets), tt.want)
-			}
-		})
+	case exp.loc != nil && target.Location == nil:
+		t.Errorf("expected %+v, got %+v", exp.loc, target.Location)
+
+	case exp.loc == nil && target.Location != nil:
+		t.Errorf("expected %+v, got %+v", exp.loc, target.Location)
+
+	case exp.loc.City != target.Location.City:
+		t.Error("expected", exp.loc.City, target.Location.City)
+		fallthrough
+	case exp.loc.Country != target.Location.Country:
+		t.Error("expected", exp.loc.Country, target.Location.Country)
+	}
+
+	// URLs
+	if len(exp.urls) != len(target.URLs) {
+		t.Fatal("expected", len(exp.urls), "got", len(target.URLs))
+	}
+	for _, expect := range exp.urls {
+		got, ok := target.URLs[expect.key]
+		if !ok {
+			t.Error("missing entry for", expect.key)
+			continue
+		}
+		samePrefix := strings.HasPrefix(got, expect.valuePrefix)
+		if !samePrefix {
+			t.Error(got, "does not start with", expect.valuePrefix)
+		}
 	}
 }
 
-func TestLocateServer_Error(t *testing.T) {
+// callback for TestLocateServer ensures the result is correct.
+func testDefaultNdt7Targets(machine string) func(t *testing.T, targets []v2.Target) {
+	return func(t *testing.T, targets []v2.Target) {
+		// Ensure the stack trace does not include this helper
+		t.Helper()
+
+		// Prepare the expectations
+		expect := &v2TargetExpectation{
+			machine:  machine,
+			hostname: machine,
+			loc: &v2.Location{
+				City:    "Ventimiglia",
+				Country: "IT",
+			},
+			urls: []v2TargetURLExpectation{
+				{
+					key:         "ws:///ndt/v7/download",
+					valuePrefix: fmt.Sprintf("ws://%s/ndt/v7/download", machine),
+				},
+				{
+					key:         "ws:///ndt/v7/upload",
+					valuePrefix: fmt.Sprintf("ws://%s/ndt/v7/upload", machine),
+				},
+				{
+					key:         "wss:///ndt/v7/download",
+					valuePrefix: fmt.Sprintf("wss://%s/ndt/v7/download", machine),
+				},
+				{
+					key:         "wss:///ndt/v7/upload",
+					valuePrefix: fmt.Sprintf("wss://%s/ndt/v7/upload", machine),
+				},
+			},
+		}
+
+		// We already checked the count is 1 so we feel entitled
+		// to just grabbing the first and only entry
+		target0 := targets[0]
+
+		// Compare with the expectations
+		expect.check(t, &target0)
+	}
+}
+
+func TestLocateServer(t *testing.T) {
+
+	// define all the test cases
 	tests := []struct {
+		// name is the name of the testcase
 		name string
-		srv  *httptest.Server
+
+		// locator is the [*LocatorV2] to use
+		locator *LocatorV2
+
+		// path is the URL path to use.
 		path string
+
+		// expectErr indicates whether we expect an error
+		expectErr bool
+
+		// expectCount is the number of expected entries
+		expectCount int
+
+		// deepCheck is an optional function for deeply checking
+		// whether the returned targets are OK.
+		deepCheck func(t *testing.T, targets []v2.Target)
 	}{
+
+		{
+			name: "success-locate-server-v2-with-domain",
+			locator: &LocatorV2{
+				TargetInfo: NewTargetInfoNdt7("mlab1-mil07.mlab-oti.measurement-lab.org"),
+			},
+			path:        "/v2/nearest",
+			expectCount: 1,
+			deepCheck:   testDefaultNdt7Targets("mlab1-mil07.mlab-oti.measurement-lab.org"),
+		},
+
+		{
+			name: "success-locate-server-v2-with-IP-address-and-port",
+			locator: &LocatorV2{
+				TargetInfo: NewTargetInfoNdt7("130.192.91.211:54321"),
+			},
+			path:        "/v2/nearest",
+			expectCount: 1,
+			deepCheck:   testDefaultNdt7Targets("130.192.91.211:54321"),
+		},
+
 		{
 			name: "error-locate-server-v2",
-			srv: NewLocateServerV2(&LocatorV2{
+			locator: &LocatorV2{
 				Err: errors.New("fake error"),
-			}),
-			path: "/v2/nearest",
+			},
+			path:      "/v2/nearest",
+			expectErr: true,
+		},
+
+		{
+			name:      "neither-error-not-target-info",
+			locator:   &LocatorV2{ /* empty */ },
+			path:      "/v2/nearest",
+			expectErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+
+			srv := NewLocateServerV2(tt.locator)
+			t.Cleanup(srv.Close)
+
 			c := locate.NewClient("fake-user-agent")
-			u, err := url.Parse(tt.srv.URL)
+			u, err := url.Parse(srv.URL)
 			testingx.Must(t, err, "failed to parse locatetest url")
 			u.Path = tt.path
 			c.BaseURL = u
 
-			ctx := context.Background()
 			// NOTE: only known services (e.g. ndt/ndt7) are supported by the locate API.
 			targets, err := c.Nearest(ctx, "ndt/ndt7")
-			if err == nil {
-				t.Errorf("expected error, got %#v", targets)
+
+			if tt.expectErr != (err != nil) {
+				t.Errorf("NewLocateServer() = expectErr %v, got %v", tt.expectErr, err)
+			}
+
+			if tt.expectCount != len(targets) {
+				t.Errorf("NewLocateServer() = expectCount %d, got %d", tt.expectCount, len(targets))
+			}
+
+			if tt.deepCheck != nil {
+				tt.deepCheck(t, targets)
 			}
 		})
 	}
